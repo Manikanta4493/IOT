9aa2846bc05fde53f783520d41aa3d0d23544b96
Manikanta4493 <143826187+Manikanta4493@users.noreply.github.com>
2025-01-18 19:24:03 +0530

    Add files via upload

---
+++ RGB LIGHTS/RGB LED BLINK.c
@@ -0,0 +1,14 @@
+void setup(){
+  pinMode(4,OUTPUT);
+  pinMode(5,OUTPUT);
+  pinMode(6,OUTPUT);
+ 
+}
+void loop(){
+    digitalWrite(4,HIGH);
+  delay(1000);
+    digitalWrite(5,HIGH);
+  delay(1000);
+    digitalWrite(6,HIGH);
+  delay(1000);
+}
